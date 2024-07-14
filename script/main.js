d43efa685690cc51b3825e7d99d32821e3416a4c
Роман <gaerroma28@gmail.com>
2024-07-14 16:41:02 +0300

    last update

--- script/main.js
+++ script/main.js
@@ -35,7 +35,7 @@ const restsFunc = () => {
             price: 900,
             raiting: 4.5,
             type: 'Пицца',
-            Image: 'image.jpg'
+            Image: '../assets/images/image.jpg'
         },
         {
             id: 2,
@@ -44,7 +44,7 @@ const restsFunc = () => {
             price: 450,
             raiting: 4.2,
             type: 'Суши и роллы',
-            Image: 'image1.jpg'
+            Image: '../assets/images/image1.jpg'
         },
         {
             id: 3,
@@ -53,7 +53,7 @@ const restsFunc = () => {
             price: 599,
             raiting: 3.8,
             type: 'Пицца и роллы',
-            Image: 'image2.jpg'
+            Image: '../assets/images/image2.jpg'
         },
         {
             id: 4,
@@ -62,7 +62,7 @@ const restsFunc = () => {
             price: 349,
             raiting: 4.7,
             type: 'Пицца и роллы',
-            Image: 'image3.jpg'
+            Image: '../assets/images/image3.jpg'
         },
         {
             id: 5,
@@ -71,7 +71,7 @@ const restsFunc = () => {
             price: 499,
             raiting: 3.5,
             type: 'Пицца и роллы',
-            Image: 'image4.jpg'
+            Image: '../assets/images/image4.jpg'
         },
         {
             id: 6,
@@ -80,7 +80,7 @@ const restsFunc = () => {
             price: 159,
             raiting: 3.6,
             type: 'Пицца и бургеры',
-            Image: 'image5.jpg'
+            Image: '../assets/images/image5.jpg'
         },
     ]
 
@@ -93,7 +93,7 @@ const restsFunc = () => {
 
         array.forEach((card) => {
             container.insertAdjacentHTML("beforeend", `
-                <a href="/tanuki.html?id=${card.id}" class="products-card">
+                <router-link to="/tanuki.html?id=${card.id}" class="products-card">
                 <div class="products-card__img">
                   <img src="assets/${card.Image}" alt="${card.Image}" />
                 </div>
@@ -119,7 +119,7 @@ const restsFunc = () => {
                     </div>
                   </div>
                 </div>
-              </a>
+              </router-link>
                 `)
         });
     }
@@ -142,37 +142,37 @@ const productFunc = () => {
             name: 'Ролл угорь стандарт',
             text: 'Рис, угорь, соус унаги, кунжут, водоросли нори.',
             price: 250,
-            image: 'tanuki1.jpg'
+            image: '../assets/images/tanuki1.jpg'
         },
         {
             name: 'Калифорния лосось стандарт',
             text: 'Рис, лосось, авокадо, огурец, майонез, икра масаго, водоросли нори.',
             price: 395,
-            image: 'tanuki2.jpg'
+            image: '../assets/images/tanuki2.jpg'
         },
         {
             name: 'Окинава стандарт',
             text: ' Рис, креветка отварная, сыр сливочный, лосось, огурец свежий...',
             price: 339,
-            image: 'tanuki3.jpg'
+            image: '../assets/images/tanuki3.jpg'
         },
         {
             name: 'Цезарь маки хl',
             text: 'Рис, куриная грудка копченая, икра масаго, томат, айсберг, соус цезарь...',
             price: 450,
-            image: 'tanuki4.jpg'
+            image: '../assets/images/tanuki4.jpg'
         },
         {
             name: 'Ясай маки стандарт 185 г',
             text: 'Рис, помидор свежий, перец болгарский, авокадо, огурец, айсберг',
             price: 359,
-            image: 'tanuki5.jpg'
+            image: '../assets/images/tanuki5.jpg'
         },
         {
             name: 'Ролл с креветкой стандарт',
             text: 'Рис, водоросли нори, креветки отварные, сыр сливочный, огурцы',
             price: 399,
-            image: 'tanuki6.jpg'
+            image: '../assets/images/tanuki6.jpg'
         },
     ]
 
